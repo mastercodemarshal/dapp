e5257f16e536867f03abc990ed9c188940c8aad8
Shankar Rao Mata <mshankarrao1970@gmail.com>
2023-03-17 09:18:16 -0400

    updating the name and removing unnecessary lines

--- api/src/frontend/tests/briefs.test.tsx
+++ api/src/frontend/tests/briefs.test.tsx
@@ -1,6 +1,7 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import React from "react";
 import { NewBrief } from "../pages/briefs/new";
+
 
 test("test  NewBrief rendering", () => {
     const briefComponent = render(<NewBrief />);
@@ -11,4 +12,11 @@ test("test  NewBrief rendering", () => {
 test("test NewBrief rendering and matching the snapshot", () => {
     render(<NewBrief />);
     expect(screen.getByText('Write a headline for your brief')).toMatchSnapshot();
+});
+
+test("test brief onclick next snapshot is matching", () => {
+    render(<NewBrief />);
+    expect(screen.getByText('Examples')).toMatchSnapshot();
+    fireEvent.change(screen.getByTestId('headline-input'), {target: {value: 'briefHeadline'}})
+    expect((screen.getByTestId('headline-input') as HTMLInputElement).value).toEqual('briefHeadline');
 });
